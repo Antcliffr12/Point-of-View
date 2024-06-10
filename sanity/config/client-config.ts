a594850b5c428ff3d4bbc86de06bef23eade0f90
Ryan Antcliff <antcliffr12@gmail.com>
2024-06-10 17:38:02 -0400

    fix Config error type of

--- sanity/config/client-config.ts
+++ sanity/config/client-config.ts
@@ -1,10 +1,15 @@
 const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!;
 const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!;
+const apiVersion = "2024-05-27";
+const perspective = 'published';
+
 const config = {
     projectId,
     dataset,
-    apiVersion: "2024-05-27",
-    perspective: 'published',
+    apiVersion,
+    useCdn: true,
 }
 
 export default config;
+
+export { perspective };
